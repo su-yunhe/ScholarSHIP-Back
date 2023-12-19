9a8a2040df15f7fc19aff35a55a0abb8daa17e7a
xfy-mufc <123456>
2023-12-19 14:41:01 +0800

    添加academic

--- ScholarSHIP/settings.py
+++ ScholarSHIP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "_(6a7b%au6%30g$fc9vf93ssme+p$@_*qd8t2y9u5d@p^09-zr"
 DEBUG = False
 
 # 修改项。允许所有的IP访问网络服务
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "test1",
     "user",
     "manager",
+    "academic",
 ]
 
 MIDDLEWARE = [
@@ -135,4 +136,4 @@ STATICFILES_DIRS = [
 ]
 
 # 新增项。静态文件收集目录
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
