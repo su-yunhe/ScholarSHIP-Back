ff16ff600a43fccd8d61f9617d9d6d464d5fc00e
maybeAlways <112628774+maybeAlways@users.noreply.github.com>
2023-12-25 22:26:35 +0800

    111

--- academic/views.py
+++ academic/views.py
@@ -32,7 +32,6 @@ def get_works(request):
     # print(articles)
     response = requests.get(f'https://api.openalex.org/works?filter=authorships.author.id:{author_id}&per_page=50&page={count}&select=abstract_inverted_index,authorships,cited_by_count,display_name,doi,id,language,primary_location,publication_date')
     articles = response.json().get('results')
-    print(articles)
     unbanned_articles = []
     banned_articles = []
     for article in articles:
@@ -40,14 +39,7 @@ def get_works(request):
         authors = []
         for author in article.get('authorships'):
             authors.append(author.get('author'))
-        i = article.get('authorships')[0].get('institutions')
-        if len(i) != 0:
-            i = i[0]
-            institution = {"id": i.get('id'), "display_name": i.get('display_name')}
-        else:
-            institution = None
         article["authors"] = authors
-        article["institution"] = institution
         article.pop("authorships")
 
         abstract_words = article.get('abstract_inverted_index')
@@ -68,7 +60,6 @@ def get_works(request):
             banned_articles.append(article)
         else:
             unbanned_articles.append(article)
-    # print(12222)
     return_articles = unbanned_articles if status == "true" else banned_articles
     return JsonResponse({"error": 0, "result": return_articles})
 
