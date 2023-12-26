60803279e0901fb50e9943ff10c15af97512b99e
maybeAlways <112628774+maybeAlways@users.noreply.github.com>
2023-12-26 16:09:45 +0800

    1111

--- academic/models.py
+++ academic/models.py
@@ -6,3 +6,8 @@ from django.db.models import *
 class Ban(Model):
     work_id = CharField(primary_key=True, max_length=100)
     author_id = JSONField()
+
+
+class BanCount(Model):
+    author_id = CharField(primary_key=True, max_length=100)
+    ban_count = IntegerField(default=1)
